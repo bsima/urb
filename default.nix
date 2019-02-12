bd128149af4f8b5cc2422535e510c99e4eb8b346
Benjamin Summers <benjamin@tlon.io>
2019-02-11 19:16:31 -0800

    Simplified nix setup.

--- default.nix
+++ default.nix
@@ -1,4 +1,17 @@
-{ pkgs ? import ./nixpkgs.nix }:
+let
+
+  rev  = "61c3169a0e17d789c566d5b241bfe309ce4a6275";
+  hash = "0qbycg7wkb71v20rchlkafrjfpbk2fnlvvbh3ai9pyfisci5wxvq";
+
+  nixpkgs = builtins.fetchTarball {
+    name = "nixpkgs-2019-01-15";
+    url = "https://github.com/nixos/nixpkgs/archive/${rev}.tar.gz";
+    sha256 = hash;
+  };
+
+in
+
+{ pkgs ? import nixpkgs {} }:
 
 let
 
@@ -13,11 +26,14 @@ pkgs.stdenv.mkDerivation rec {
   unpackPhase  = "true";
   installPhase = ''
     mkdir -p $out/bin
+
     cp ${./urb} $out/bin/urb.py
+
     cat > $out/bin/urb <<EOF
     #!/usr/bin/env bash
     ${pyexe} $out/bin/urb.py "\$@"
     EOF
+
     chmod +x $out/bin/urb
   '';
 }
